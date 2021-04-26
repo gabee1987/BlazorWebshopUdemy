78070edf8940ad1f0e7f0230fadb40a232156d4e
Gabor Koncz <gabee1987@gmail.com>
2021-04-26 17:42:03 +0200

    created the business logic project

---
+++ eShop.CoreBusiness/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace eShop.CoreBusiness.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
