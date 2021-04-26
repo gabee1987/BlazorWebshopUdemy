e2cf1388f9a7129261eb531659fbd3d7564736f5
Gabor Koncz <gabee1987@gmail.com>
2021-04-26 18:04:08 +0200

    added data store plugin interfaces

---
+++ eShop.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,22 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class SearchProduct
+    {
+        private readonly IProductRepository m_ProductRepository;
+        public SearchProduct( IProductRepository productRepository )
+        {
+            this.m_ProductRepository = productRepository;
+        }
+
+        public IEnumerable<Product> Execute( string filter )
+        {
+           return m_ProductRepository.GetProducts( filter );
+        }
+    }
+}
---
+++ eShop.UseCases/SearchProductScreen/ViewProduct.cs
@@ -0,0 +1,22 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class ViewProduct
+    {
+        private readonly IProductRepository m_ProductRepository;
+        public ViewProduct( IProductRepository productRepository )
+        {
+            this.m_ProductRepository = productRepository;
+        }
+
+        public Product Execute( int id )
+        {
+            return m_ProductRepository.GetProduct( id );
+        }
+    }
+}
