f1b7643e1c27fadac3fdb9a88d6cfebd65f4192e
Gabor Koncz <gabee1987@gmail.com>
2021-04-30 12:47:29 +0200

    implemented form example with validations

---
+++ eShop.CoreBusiness/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace eShop.CoreBusiness.Models
+{
+    public class Customer
+    {
+        [Required]
+        [StringLength( 100 )]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength( 100 )]
+        public string LastName { get; set; }
+        [Required]
+        [Range( 1, 1000 )]
+        public int CustomerNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
